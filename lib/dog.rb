0bc216bcc7c03a39fbc3da983dfa128c4155d2fa
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-11 05:58:55 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  
+  def initialize(dog_name, dog_breed)
+    @name = dog_name
+    @breed = dog_breed
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
